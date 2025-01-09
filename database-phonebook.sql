884f768d57580262b1db63e88dd672db4d22932f
Maheen Fatima <s.maheenfatima16@gmail.com>
2025-01-10 01:18:38 +0500

    vid 5

---
+++ database-phonebook.sql
@@ -0,0 +1,22 @@
+CREATE TABLE login(
+	username varchar(20),
+    password varchar(16)
+);
+INSERT INTO login (username, password)
+VALUES
+("admin","maheen");
+
+CREATE TABLE add_contact (
+	id int primary key auto_increment,
+    name varchar(50),
+    nickname varchar(50),
+    phone varchar(11),
+    mobile varchar(11),
+    email varchar(50),
+    address varchar(255),
+    company varchar(50),
+    position varchar(50),
+    group_name varchar(50)
+);
+SELECT * FROM add_contact;
+
